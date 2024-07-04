854ad16c66522668aa622000a0f0297da47bc19b
cash-i1 <cashignitro@gmail.com>
2024-07-05 08:36:18 +1000

    added basic styles and webpage

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,15 @@ fn App() -> impl IntoView {
 }
 
 #[component]
-fn Home() -> impl IntoView {}
+fn Home() -> impl IntoView {
+    view! {
+        <div class="root">
+            <div class="center-box">
+                <h1>diet tracker</h1>
+            </div>
+        </div>
+    }
+}
 
 fn main() {
     mount_to_body(move || view !{ <App/> })
